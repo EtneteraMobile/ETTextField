cc8592722643194bdc7ccab6c5b9c1432f4e31b2
Miroslav Beles <miroslav.beles@etnetera.cz>
2019-08-15 08:22:19 +0200

    ✨ SwiftPM dependencies

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/EtneteraMobile/ETBinding.git", from: "3.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
