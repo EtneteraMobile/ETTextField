cf706c78db61e13756da6143c60c0bf0817f9eae
Marek Šťovíček <marek.stovicek@etnetera.cz>
2021-04-14 19:37:12 +0200

    Add ETBinding as dependency to ETTextField target

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ETTextField",
-            dependencies: []),
+            dependencies: ["ETBinding"]),
         .testTarget(
             name: "ETTextFieldTests",
             dependencies: ["ETTextField"])
